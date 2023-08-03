be205e2aa9d7219425cbac67be978b635e3cbd72
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-03 23:16:25 +0200

    initial commit

---
+++ app/src/main/java/com/genuinecoder/springclient/model/Breakedown.java
@@ -0,0 +1,51 @@
+package com.genuinecoder.springclient.model;
+
+public class Breakedown {
+
+  private int id;
+  private String failureName;
+  private String computerName;
+  private String description;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFailureName() {
+    return failureName;
+  }
+
+  public void setFailureName(String failureName) {
+    this.failureName = failureName;
+  }
+
+  public String getComputerName() {
+    return computerName;
+  }
+
+  public void setComputerName(String computerName) {
+    this.computerName = computerName;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+        "id=" + id +
+        ", name='" + failureName + '\'' +
+        ", location='" + computerName + '\'' +
+        ", branch='" + description + '\'' +
+        '}';
+  }
+}
---
+++ app/src/main/java/com/genuinecoder/springclient/reotrfit/BreakdownApi.java
@@ -0,0 +1,18 @@
+package com.genuinecoder.springclient.reotrfit;
+
+import com.genuinecoder.springclient.model.Breakedown;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface BreakdownApi {
+
+  @GET("/findall")
+  Call<List<Breakedown>> getAllEmployees();
+
+  @POST("/breakdown")
+  Call<Breakedown> save(@Body Breakedown breakedown);
+}
