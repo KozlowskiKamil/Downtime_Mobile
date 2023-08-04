5c9f544c0b2b940a4bff03de2fdbb5bf1872b8b9
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-05 01:53:11 +0200

    add method

--- app/src/main/java/com/downtime/mobile/NavbarActivity.java
+++ app/src/main/java/com/downtime/mobile/NavbarActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.downtime.mobile.databinding.ActivityMain2Binding;
+
 public class NavbarActivity extends AppCompatActivity {
+
+    public ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navbar);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.failure:
+                    break;
+                case R.id.done:
+                    break;
+                case R.id.close:
+                    break;
+                case R.id.etToken:
+                    break;
+            }
+
+
+
+            return true;
+        });
+
+
+
     }
 }
