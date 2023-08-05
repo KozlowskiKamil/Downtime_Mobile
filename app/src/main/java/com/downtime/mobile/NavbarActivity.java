4379e1952b73005c7798d259cf3e27304e7e6e4c
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-05 02:02:04 +0200

    add method

--- app/src/main/java/com/downtime/mobile/NavbarActivity.java
+++ app/src/main/java/com/downtime/mobile/NavbarActivity.java
@@ -1,8 +1,11 @@
 package com.downtime.mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.downtime.mobile.databinding.ActivityMain2Binding;
 
@@ -15,26 +18,40 @@ public class NavbarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replacefragment(new FailureFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
             switch (item.getItemId()) {
                 case R.id.failure:
+                    replacefragment(new FailureFragment());
                     break;
                 case R.id.done:
+                    replacefragment(new DoneFragment());
+
                     break;
                 case R.id.close:
+                    replacefragment(new CloseFragment());
+
                     break;
                 case R.id.etToken:
+                    replacefragment(new TokenFragment());
+
                     break;
             }
-
 
 
             return true;
         });
 
-
-
     }
+
+    private void replacefragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
