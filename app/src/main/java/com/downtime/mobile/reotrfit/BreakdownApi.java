8d150e744c30e94e8699212ed7361602833e725d
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-13 02:11:30 +0200

    add ended ongoing samefailure methods

--- app/src/main/java/com/downtime/mobile/reotrfit/BreakdownApi.java
+++ app/src/main/java/com/downtime/mobile/reotrfit/BreakdownApi.java
@@ -7,7 +7,9 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface BreakdownApi {
 
@@ -16,4 +18,17 @@ public interface BreakdownApi {
 
     @POST("/breakdown")
     Call<Breakedown> save(@Body Breakedown breakedown);
+
+    @GET("/ended")
+    Call<List<Breakedown>> getEnded();
+
+    @GET("/ongoing")
+    Call<List<Breakedown>> getOngoing();
+
+    @POST("/samefailure/{computerName}/{failureName}")
+    Call<Breakedown> findAllByComputerNameAndFailureName(@Path("computerName") String computerName,
+                                                         @Path("failureName")String failureName);
+
+
+
 }
