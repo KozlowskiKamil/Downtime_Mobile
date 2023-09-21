9e6ab87ce79c1812a20bb6bee7cd218ee8d9e8b4
KozlowskiKamil <kamil2k8@gmail.com>
2023-09-21 23:46:38 +0200

    new ip

--- app/src/main/java/com/downtime/mobile/reotrfit/RetrofitService.java
+++ app/src/main/java/com/downtime/mobile/reotrfit/RetrofitService.java
@@ -9,9 +9,10 @@ public class RetrofitService {
 
     private Retrofit retrofit;
 
-//    public static final String IP_ADDRESS = "http://192.168.1.53:8080"; // TODO: 03.08.2023 Konfiguracja IP
+    public static final String IP_ADDRESS = "http://kozlowski.alwaysdata.net"; // TODO: 03.08.2023 Konfiguracja IP
 //    public static final String IP_ADDRESS = "http://192.168.43.171:8080"; // TODO: 03.08.2023 Konfiguracja IP
-    public static final String IP_ADDRESS = "http://192.168.157.7:8080"; // TODO: 03.08.2023 Konfiguracja IP
+//    public static final String IP_ADDRESS = "http://192.168.157.7:8080"; // TODO: 03.08.2023 Konfiguracja IP
+//    public static final String IP_ADDRESS = "http://192.168.0.240:8080"; // TODO: 03.08.2023 Konfiguracja IP
 
     public RetrofitService() {
         initializeRetrofit();
